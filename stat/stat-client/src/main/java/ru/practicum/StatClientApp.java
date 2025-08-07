c90ca8c4133da3262cfec7c1935e380f66255ce4
NadezhdaTA <nadezda.tupitsyna@yandex.ru>
2025-08-07 16:34:42 +0400

    Настройки docker и pom.xml

---
+++ stat/stat-client/src/main/java/ru/practicum/StatClientApp.java
@@ -0,0 +1,10 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class StatClientApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ stat/stat-dto/src/main/java/ru/practicum/StatDtoApp.java
@@ -0,0 +1,11 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class StatDtoApp {
+    public static void main(String[] args) {
+
+    }
+}
